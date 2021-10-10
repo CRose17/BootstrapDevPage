1b52f262c45a5c0fb939f5bb80cb482e734a5786
Cameron Rosenberger <crosenberger17@gmail.com>
2021-10-10 11:54:04 -0700

    navbar dropdown and email links set up

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// Select the button from the markup
+const emailButton = document.querySelector(".click-to-copy");
+
+// Function that runs on click. It:
+// 1) Prevents the default behavior of the button (refresh the page);
+// 2) Runs the copyToClipboard function;
+// 3) Adds and removes some CSS classes, used for styling and notifying the user about the copy event
+const clickToCopy = (e) => {
+  e.preventDefault();
+  copyToClipboard(e.currentTarget.textContent);
+  e.target.classList.add("is-copied");
+  setTimeout(() => {
+    e.target.classList.remove("is-copied");
+  }, 1200);
+};
+
+// Copy to clipboard function, taken from https://www.30secondsofcode.org/blog/s/copy-text-to-clipboard-with-javascript/
+const copyToClipboard = (str) => {
+  const el = document.createElement("textarea");
+  el.value = str;
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false;
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+};
+
+// Fire the event on click
+emailButton.addEventListener("click", clickToCopy);
+
+//  scroll down hide navbar - scroll up show navbar
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style = "top: 0; transition: 0.5s";
+  } else {
+    document.getElementById("nav").style = "top: -120px; transition: 0.5s";
+  }
+  prevScrollpos = currentScrollPos;
+};
